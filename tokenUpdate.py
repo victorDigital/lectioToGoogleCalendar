7913cc59ccd8f93c61b48c9406edd8ab80e84c19
victor <victordj587@gmail.com>
2022-09-25 22:25:43 +0200

    added basic functionality, README coming soon

---
+++ tokenUpdate.py
@@ -0,0 +1,30 @@
+import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+
+def tokenUpdate():
+    creds = Credentials(
+        token=os.environ["TOKEN"],
+        refresh_token=os.environ["REFRESH_TOKEN"],
+        token_uri=os.environ["token_uri"], 
+        client_id=os.environ["client_id"],
+        client_secret=os.environ["client_secret"],
+    )
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('calendar', 'v3', credentials=creds)
